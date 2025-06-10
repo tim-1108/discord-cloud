5f2bb7b4f7135147962ad25f7f9fb47278a5ffd8
Tim <timthiede1@gmail.com>
2025-06-10 13:21:56 +0200

    chore: more debugging for discord webhook errors

--- upload-service/discord.ts
+++ upload-service/discord.ts
@@ -4,6 +4,7 @@ import type { Message } from "../common/discord.js";
 import { getEnvironmentVariables } from "../common/environment.js";
 import axios, { AxiosError } from "axios";
 import { sleep } from "../common/useless.js";
+import { logError } from "../common/logging.js";
 
 export async function sendWebhookMessage(attachment: Buffer, index: number, message: string, attemptCounter: number = 0) {
     const url = getWebhookURL();
@@ -25,11 +26,13 @@ export async function sendWebhookMessage(attachment: Buffer, index: number, mess
         const data = response.data as Partial<Message>;
         // Acts as a fallback
         if (response.status !== 200) {
+            logError("Failed to send message", response.status, response.data);
             return null;
         }
         return typeof data.id === "string" && typeof data.channel_id === "string" ? { id: data.id, channel_id: data.channel_id } : null;
     } catch (error) {
         if (!(error instanceof AxiosError)) return null;
+        logError("Axios Error:", error, error.response);
         if (error.status === 429 && attemptCounter < 5) {
             const retryAfterSeconds = parseFloat(error.response?.data?.retry_after) || 1;
             await sleep(retryAfterSeconds * 1000);
